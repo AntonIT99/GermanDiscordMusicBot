5138b0764ac050327a88fa312379aaab90deb2ec
AntonIT99 <43090912+AntonIT99@users.noreply.github.com>
2024-05-18 02:34:34 +0200

    ffmpeg options for more reliable streaming

--- music_source.py
+++ music_source.py
@@ -20,7 +20,7 @@ ytdl_options = {
 }
 
 ffmpeg_options = {
-    # 'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', # for ffmpeg version >= 5, does not work on ffmpeg version 4
+    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', # for ffmpeg version >= 5 only, does not work on ffmpeg version 4
     'options': '-vn'
 }
 
